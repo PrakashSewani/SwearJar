aacde89d860ca5571b2cd1b2111320feec2809e3
PrakashSewani <adamjones2227@gmail.com>
2021-04-27 01:09:06 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,52 @@
+import discord
+import os
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.lower().startswith('$hello'):
+        await message.reply('Hello! <@' + str(message.author.id) + '>')
+
+    if message.content.lower().startswith('$add'):
+        with open('blacklist.txt', 'r') as f:
+            words = f.read()
+            blacklist = words.split()
+
+        msg = message.content.lower()
+        for word in blacklist:
+            if word in msg:
+                await message.reply(message.content[5:] + ' already exists in the blacklist <@' + str(message.author.id) + '>')
+                return
+
+        with open("blacklist.txt", "a") as writefile:
+            writefile.write(' ' + message.content[5:])
+
+        with open('blacklist.txt', 'r') as f:
+            words = f.read()
+            blacklist = words.split()
+
+        await message.reply('Added <@' + str(message.author.id) + '>')
+        return
+
+    with open('blacklist.txt', 'r') as f:
+        words = f.read()
+        blacklist = words.split()
+    
+    msg = message.content.lower()
+
+    for word in blacklist:
+        if word in msg:
+            await message.reply('Don\'t swear <@' + str(message.author.id) + '>')
+            await message.delete()
+            return
+    return
+    
+client.run('ODI0MTcxODUwMTkwOTQ2MzA0.YFrf2Q.LcMsocF0yT9poZ2-MFqMBdtHX_k')
